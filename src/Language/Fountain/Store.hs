c832236e0925adf6d88c4c41147166f2f8cbdf8d
Chris Pressey <cpressey@catseye.tc>
2023-05-07 13:08:16 +0100

    fetch cascades; pushScope, popScope.

--- src/Language/Fountain/Store.hs
+++ src/Language/Fountain/Store.hs
@@ -13,12 +13,22 @@ data Store = Store {
 
 
 empty = Store{ scopes=[Map.empty], events=[] }
-fetch k st = Map.lookup k (head $ scopes st)
-insert k v st = st{ scopes=insertTop k v $ scopes st, events=("insert":events st) }
-update f k st = st{ scopes=updateTop f k $ scopes st, events=("update":events st) }
 
-insertTop k v (table:rest) = (Map.insert k v table:rest)
-updateTop f k (table:rest) = (Map.update f k table:rest)
+fetch k st = lookup k $ scopes st where
+    lookup k [] = Nothing
+    lookup k (table:rest) =
+        case Map.lookup k table of
+            Just v -> Just v
+            Nothing -> lookup k rest
+
+insert k v st = st{ scopes=insertTop k v $ scopes st, events=("insert":events st) }
+    where insertTop k v (table:rest) = (Map.insert k v table:rest)
+
+update f k st = st{ scopes=updateTop f k $ scopes st, events=("update":events st) }
+    where updateTop f k (table:rest) = (Map.update f k table:rest)
+
+pushScope st = st{ scopes=(Map.empty:scopes st) }
+popScope st = st{ scopes=tail $ scopes st }
 
 constructStore :: [String] -> Store
 constructStore [] = empty
